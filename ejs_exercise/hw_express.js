ed28453f23213e669182f747f44fd42c9b44eae5
DavideDaniel <davidedaniel@gmail.com>
2015-01-14 09:45:43 -0500

    hw

--- ejs_exercise/hw_express.js
+++ ejs_exercise/hw_express.js
@@ -1,23 +1,29 @@
-var express = require('express')
-var ejs = require( 'ejs' );
-var fs = require('fs')
+var express = require( "express" )
+var ejs = require( "ejs" );
+var fs = require( "fs" )
 var app = express();
 
+var name = [ "Alex", "David", "Jason" ];
+var home = [ "Lynbrook", "Brooklyn", "Flushing" ];
+var colors = [ "Blue", "Green", "Black" ];
+var bands = [ "Linkin Park", "Led Zepplin",
+	"Chvrches"
+];
 
-app.get('/:id1/:id2', function(req, res){
-	var width = parseInt(req.params.id1)
-  	var height = parseInt(req.params.id2)
+app.get( "/", function ( req, res ) {
+	var ran_name = name[ Math.floor( Math.random() * name.length ) ]
+	var ran_home = home[ Math.floor( Math.random() * home.length ) ]
+	var ran_color = colors[ Math.floor( Math.random() * colors.length ) ]
+	var ran_band = bands[ Math.floor( Math.random() * bands.length ) ]
 
-		var str = fs.readFileSync( 'views/kittensJS.ejs', 'utf8' );
-		var rendered = ejs.render( str, {
-				width: width, height: height }
-				)
+	var str = fs.readFileSync( "views/hwIndex.ejs", "utf8" );
+	var rendered = ejs.render( str, {
+		name: ran_name,
+		home: ran_home,
+		band: ran_band,
+		color: ran_color
+	} );
+	res.send( rendered );
+} );
 
-			res.send( rendered );
-		} );
-
-app.listen(3000);
-
-console.log('listening on port 3000!');
-
-
+app.listen( 3000 );
